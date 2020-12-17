db14291a0b3c14e6459d5b6d477c4ee9f728904b
B.Mady <kaba.bintou-mady_fr@guj.de>
2020-12-17 21:41:08 +0100

    added to the repo

---
+++ src/views/Tables2/LineFinancialsProps.js
@@ -0,0 +1,110 @@
+import React from 'react'
+
+import "react-datepicker/dist/react-datepicker.css";
+import {rowStyle, theme} from "../base/Tree/BasicTreeTableProps";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+export const styles = {
+  outer: {
+    borderRadius: 5,
+    boxShadow: "0 10px 30px #BBB",
+    padding: 10
+  },
+  spacer: {
+    flex: '1 1 10%',
+  },
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  }
+};
+
+const mapping = (acc) =>
+    <MenuItem key={acc.id} value={acc.id}>
+        {acc.id.concat( " ").concat(acc.name)}
+    </MenuItem>
+
+
+export  const accountD=(data, value, onRowDataChange, rowData) => (
+      <Select
+          value={value}
+          onChange={(event) => {
+            onRowDataChange({
+              ...rowData,
+              account: (event.target.value)
+            });
+          }}
+      >
+        {data.map(mapping)}
+      </Select>
+  )
+export const accountC =(data, value, onRowDataChange, rowData) => (
+      <Select
+          value={value}
+          onChange={(event) => {
+            onRowDataChange({
+              ...rowData,
+              oaccount: (event.target.value)
+            });
+          }}
+      >
+        {data.map(mapping)}
+      </Select>
+  )
+
+
+export const  editable = (current, setCurrent, setData ) => ({
+   /* onRowAdd: newData =>
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+           // setData([...current, newData]);
+
+            resolve();
+          }, 1000)
+        }),
+
+    */
+        onRowUpdate: (newData, oldData) =>
+        new Promise((resolve, reject) => {
+          console.log('newData', newData)
+          console.log('oldData', oldData)
+          setTimeout(() => {
+            const currentx = {...current};
+            const index = currentx.lines.findIndex(obj => obj.lid === newData.lid);
+            currentx.lines[index] = newData;
+            console.log('currentxX', currentx)
+            setCurrent({...currentx});
+            //console.log('currentxXX', current)
+            resolve()
+          }, 1000)
+        }),
+        onRowDelete : oldData =>
+        new Promise((resolve, reject) => {
+          setTimeout(() => {
+            const dataDelete = [...current.lines];
+            const index = oldData.tableData.id;
+            dataDelete.splice(index, 1);
+            setData([...dataDelete]);
+            resolve()
+          }, 1000)
+        })
+})
+
+export const Linescolumns =(data, line, current, t) => [
+      {field:'lid', title:t('financials.line.id'), hidden:true}
+    , {field:'transid', title:"transid", hidden:true, initialEditValue:current.tid}
+    , {field:'account', title:t('financials.line.account'), editComponent:({ value, onRowDataChange, rowData }) =>
+          accountD ( data, value, onRowDataChange, rowData ), width: 20}
+    , {field:'side', title:t('financials.line.side'), type:"boolean", initialEditValue:true, width:10}
+    , {field:'oaccount', title:t('financials.line.oaccount'), editComponent:({ value, onRowDataChange, rowData }) =>
+          accountC ( data, value, onRowDataChange, rowData ), width: 20}
+    , {field:'duedate', title:t('financials.line.duedate'), type:"date", align:"right",
+      initialEditValue:line.duedate,
+      dateSetting: { locale:"de" } }
+    , {field:'amount', title:t('financials.line.amount'), type:"currency",
+       currencySetting: { locale:"de", currencyCode: "EUR", minimumFractionDigits: 2, maximumFractionDigits: 2 }}
+    , {field:'text', title:t('financials.line.text'), width: 300}
+  ]
+
+
+
