56fadb168af8b9633fa9715bd93ebd7d8a90e138
B.Mady <kaba.bintou-mady_fr@guj.de>
2021-01-01 10:09:27 +0100

    removed unnecessary log stmt

--- src/views/base/Components/AccountContext.js
+++ src/views/base/Components/AccountContext.js
@@ -2,10 +2,7 @@ import React, {createContext} from 'react'
 import {createGlobalState} from "react-hooks-global-state";
 import {useTranslation} from "react-i18next";
 
-const initialState = {profile:{
-    token:'noTOken'
-    , company:''
-    }}
+const initialState = {profile:{token:'noTOken', company:''}}
 
 export const { useGlobalState } = createGlobalState(initialState);
 export const accountContext = createContext(null);
@@ -14,13 +11,7 @@ export  const AccountContext = (props) => {
     const { t,  } = useTranslation();
     const data = {hits:[]};
     const accData = props.initAcc;
-    const ccData = props.initCc
-
-  console.log('props.url', props.url );
-  console.log('props.accUrl', props.accUrl );
-  console.log('initialState', props.initialState );
-  console.log('data', data );
-  console.log('accData', accData );
+    const ccData = props.initCc;
 
   return (
     <accountContext.Provider value={{data:data, accData:accData, ccData:ccData, user:props.current, editing:props.editing
