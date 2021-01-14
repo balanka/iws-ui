f57b3d9766bd66aebb86846c8eeb2cd8be776ecf
B.Mady <kaba.bintou-mady_fr@guj.de>
2021-01-14 09:54:25 +0100

    imported TableInons and replaced the existing ones

--- src/views/Tables2/EditableTable.js
+++ src/views/Tables2/EditableTable.js
@@ -1,39 +1,18 @@
 import React from 'react';
 import MaterialTable from '@material-table/core';
-import Add from "@material-ui/icons/Add";
-import Clear from "@material-ui/icons/Clear";
-import Check from "@material-ui/icons/Check";
-import FirstPage from "@material-ui/icons/FirstPage";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import LastPage from "@material-ui/icons/LastPage";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import Search from "@material-ui/icons/Search";
-import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
-import RateReviewIcon from '@material-ui/icons/RateReview';
+import tableIcons from "./TableIcons.js";
 
 export default function EditableTable(props) {
     const {Options, flag, data, columns, t, tableRef, edit, editable, setSelectedRows} = props
-    const icons ={
-        Add: () => <Add />,
-            Check: () => <Check />,
-            Clear: () => <Clear />,
-            Delete: () => <IndeterminateCheckBoxIcon />,
-            Edit: () => <RateReviewIcon />,
-            FirstPage: () => <FirstPage />,
-            NextPage: () => <ChevronRight />,
-            LastPage: () => <LastPage />,
-            PreviousPage: () => <ChevronLeft />,
-            ResetSearch: () => <Clear />,
-            Search: () => <Search />
-    }
+
     return (
         <MaterialTable
             editable={flag?null:editable}
-           tableRef={tableRef}
+            tableRef={tableRef}
             //title={props.title}
             columns={columns}
             data={data}
-            icons={icons}
+            icons={tableIcons}
             options={Options}
             //components={components}
             onSelectionChange={(rows) => {
