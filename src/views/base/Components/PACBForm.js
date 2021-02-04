7d9963c1c22ffe521dd85f28ed1dc73539d8070d
B.Mady <kaba.bintou-mady_fr@guj.de>
2021-02-04 10:33:29 +0100

    refactored and replaced enhancedTable with EditableTable

--- src/views/base/Components/PACBForm.js
+++ src/views/base/Components/PACBForm.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState, useContext} from 'react'
-import {accountContext, useGlobalState} from './AccountContext';
+import React, {useState, useContext} from 'react'
+import {accountContext} from './AccountContext';
 import useFetch from "../../../utils/useFetch";
 import Grid from "react-fast-grid";
 import {useTranslation} from "react-i18next";
 import {FormFactory,JournalFormHead} from './FormsProps'
-import {columnsPACB, filter, OptionsM} from "../../Tables2/LineFinancialsProps";
+import {columnsPACB, OptionsM} from "../../Tables2/LineFinancialsProps";
 import {formEnum} from "../../../utils/FORMS";
 import blue from "@material-ui/core/colors/blue";
 import {styles, theme} from "../Tree/BasicTreeTableProps";
@@ -12,9 +12,8 @@ import EditableTable from "../../Tables2/EditableTable";
 const PACBForm = () => {
   const { t,  } = useTranslation();
   const [state, setState]= useState({collapse: true, fadeIn: true, timeout: 300});
-  const [selected, setSelected] = useState([]);
-  const [profile, ] = useGlobalState('profile');
-  const [ , setUrl] = useState('');
+
+  const [isDebit, setIsDebit ] = useState(true);
   const value = useContext(accountContext);
   const [{ res}, ]= useFetch(value.url, {});
   const [{ res2},] = useFetch(value.accUrl, {});
@@ -30,7 +29,6 @@ const PACBForm = () => {
   const [toolbar, setToolbar] = useState(true);
 
   const columnsX= columnsPACB(t);
-  const getColumnName = ()=>columnsX.map(col =>col.field);
   const toggleToolbar= ()=> setToolbar(!toolbar );
   const toggle= ()=> setState({...state, collapse:!state.collapse });
 
@@ -39,48 +37,38 @@ const PACBForm = () => {
         .concat(current.fromPeriod).concat('/')
         .concat(current.toPeriod);
 
-    const dx=data?.hits?data?.hits:value.initialState
-    const [filteredRows, setFilteredRows] = useState(dx);
-    useEffect(() => {handleFilter()}, [data, handleFilter]);
-
-    function handleFilter(text) {
-       const rows_=text?filter(getData(), getColumnName(), text ):getData()
-       setFilteredRows(rows_);
-    }
     const summary =(data)=> {
         const row_=data;
         const row = row_?.hits?row_?.hits?.slice():row_.slice();
         let debit=0, credit=0, balance=0
         let currency=''
         let company=''
+
         const available=row.length>0
         for(let i = 0, len = row.length-1; i <= len; ++i) {
             debit=debit+row[i].debit
             credit=credit+row[i].credit
-            balance=row[i].isDebit? (balance + debit-credit):(balance+credit-debit)
+            balance=isDebit? (balance + debit-credit):(balance+credit-debit)
+              const runningBalance = isDebit? (row[i].debit+row[i].idebit-row[i].credit-row[i].icredit):
+                (row[i].credit+row[i].icredit-row[i].debit-row[i].idebit)
             currency = row[i].currency
             company = row[i].company
+            row[i] = {...row[i], balance:runningBalance}
         }
         const len=row.length
         row[len] = {period:"Total", idebit:available?row[0].idebit:0, debit:debit, icredit:available?row[0].icredit:0
             , credit:credit, balance:balance, currency:currency,company:company  }
         return row
     }
-    const getFilteredRows=()=>{
-        const row_=filteredRows?filteredRows:getData();
-        const row = row_?.hits?row_?.hits?.slice():row_.slice();
-        for(let i = 0, len = row.length-1; i <= len; ++i) {
-            row[i] = {...row[i], balance: row[i].idebit + row[i].debit - (row[i].icredit + row[i].credit)};
-         }
-        //console.log('rowx', row);
-        return row;
-    }
+
     const load = event => {
         event.preventDefault();
         accData?.hits?.length<2?
             value.submitQuery(event, value.accUrl, setAccData, value.initAcc):
             current.account&&current.fromPeriod&&current.toPeriod?
             value.submitQuery(event, url_(), setData, value.initialState): void(0)
+        setIsDebit(accData.hits.find(x=>x.id===current.account).isDebit)
+        console.log("accDataX", isDebit);
     };
 
 
@@ -89,14 +77,8 @@ const PACBForm = () => {
         accData?.hits?.length<2?
             value.submitQuery(event, value.accUrl, setAccData,value.initAcc):
             value.submitQuery(event, url_(), setData, value.initialState)
+        console.log("accDataX", accData);
     };
-
-
-  const submitEdit = event => {
-    event.preventDefault();
-    //console.log("submitEdit1 current", current);
-    setUrl(url_());
-  };
 
 
   function buildForm(){
@@ -123,8 +105,4 @@ const PACBForm = () => {
 
 };
 export default PACBForm;
-//<EnhancedTable props={props} style={{padding: 0, height: 50}}/>
-//<EditableTable Options={{...OptionsM, selection:false, toolbar:toolbar}}  data={data?.hits?data.hits:value.initialState.hits}
-//               columns={columns}   theme={theme} t={t}  reducerFn ={reducerFn} renderTotal={renderSummaryRow}
-//               setSelectedRows ={()=>void(0)}/>
-//
+
