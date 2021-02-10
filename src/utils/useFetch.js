10922f793676495526351696a721002cab531323
B.Mady <kaba.bintou-mady_fr@guj.de>
2021-02-10 22:04:19 +0100

    removed unnecessary comments

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -6,44 +6,24 @@ const useFetch = (url, data, options) => {
     const [response, setResponse] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [profile, setProfile] = useGlobalState('profile');
+    const [profile, ] = useGlobalState('profile');
     const value = useContext(accountContext);
-   // console.log('urlX', url);
-
-    const submitGet = (url) => {
-        //console.log('authorization2', profile.token);
-        axios.get( url, {headers: {'authorization':profile.token}})
-            .then(response => {
-                console.log('response.data', response.data);
-                console.log('response.headers', response.headers);
-                const resp = response.data
-                return resp;
-            }).catch(function (error) {
-            console.log('error', error);
-        });
-    }
 
     useEffect(() => {
           const fetchData =  () => {
-            console.log('Fetching data for ',url);
               setLoading(true);
               setResponse(null);
               setError(null);
-              console.log('setLoading',loading);
-              console.log('options', options)
             try {
                 //if(!loading){
                     //const res = await fetch(url, options);
                     //const json = await res.json();
-                    const json = profile.token==='noTOken'? value.login(url,{}):value.submitGet(url, setResponse)
-                  console.log('FetchedDataX',json);
-                  console.log('response',response);
+                  const json = profile.token==='noTOken'? value.login(url,{}):value.submitGet(url, setResponse)
                   if(json) setResponse(json);
                     setLoading(false);
                // }
             } catch (error) {
                 console.log('errorX',error);
-                console.log('profile.token',profile.token);
                 setError(error);
                 setLoading(false);
             }
