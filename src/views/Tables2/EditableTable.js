4d3d26d210887547d1d330d1c61607b290595e42
B.Mady <kaba.bintou-mady_fr@guj.de>
2021-07-17 23:31:01 +0200

    Updated replaced material-table with material-table/core

--- src/views/Tables2/EditableTable.js
+++ src/views/Tables2/EditableTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import MaterialTable from 'material-table';
+import {MaterialTable} from '@material-table/core';
 import tableIcons from "./TableIcons.js";
 
 export default function EditableTable(props) {
