8ce48057f25fd6468d684e4e9f30ae9dcf04751e
balanka <batexy@gmail.com>
2023-06-16 09:15:30 +0200

    Bug fix

--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import 'react-app-polyfill/ie11'; // For IE 11 support
 import 'react-app-polyfill/stable';
 import './polyfill'
 import React from 'react';
-//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './i18n';
-
 import { icons } from './assets/icons'
-
 import { Provider } from 'react-redux'
 import store from './store'
 
 React.icons = icons
 
 const container = document.getElementById('root');
+
 render(
     <React.StrictMode>
        <Provider store={store}>
@@ -25,14 +24,6 @@ render(
     console.log('rendered');
 });
 
-/*ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App/>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);*/
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
