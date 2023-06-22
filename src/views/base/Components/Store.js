62f1d8c1f19617bdd85544e73c588e876ed3e4ae
balanka <batexy@gmail.com>
2023-06-22 17:26:24 +0200

    added update method 4 updating objects in the stores

--- src/views/base/Components/Store.js
+++ src/views/base/Components/Store.js
@@ -1,4 +1,4 @@
-import { Subject } from 'rxjs';
+import {Subject} from 'rxjs';
 
 const subject = new Subject();
 const initialState = new Map();
@@ -6,34 +6,44 @@ const initialState = new Map();
 
 let store = initialState;
 const iwsStore = {
-    init: ()=> {
-        console.debug('store',store);
-        store = new Map([...store.entries()]);
-        subject.next(store);
-    },
-    subscribe: setState => subject.subscribe(setState),
-    put: (key, message) => {
-        console.debug('key',key);
-        console.debug('message',message);
-        console.debug('store',store);
-        console.debug('instanceof',(store instanceof Map));
-        const temp = (store instanceof Map)?store.get(key):[];
-        console.debug('temp',temp);
-        const temp1 =temp?temp:[];
-        console.debug('temp1',temp1);
-        console.debug('Array.isArray(temp1)',Array.isArray(temp1));
-        console.debug('spread (temp1)',[...temp1, ...message]);
-        const temp2 = Array.isArray(temp1)&&temp1.length>0?[...temp1]:[...message];
-        console.debug('temp2',temp2);
-        const cpy = store.set(key, temp2);
-        store = new Map([...store.entries()]);
-        console.debug('store',store);
-        subject.next(store);
-    },
-    clear: () =>{
-        store = initialState;
-        subject.next(store);
-    },
-    initialState
+  init: () => {
+    store = new Map([...store.entries()]);
+    subject.next(store);
+  },
+  subscribe: setState => subject.subscribe(setState),
+  put: (key, message) => {
+    console.debug('key', key);
+    console.debug('message', message);
+    console.debug('store', store);
+    console.debug('instanceof', (store instanceof Map));
+    const temp = (store instanceof Map) ? store.get(key) : new Set();
+    console.debug('temp', temp);
+    const temp1 = temp ? temp : [];
+    console.debug('temp1', temp1);
+    console.debug('Array.isArray(temp1)', Array.isArray(temp1));
+    console.debug('spread (temp1)', [...temp1, ...message]);
+    const temp2 = Array.isArray(temp1) && temp1.length > 0 ? [...temp1] : [...message];
+    console.debug('temp2', temp2);
+    const cpy = store.set(key, temp2);
+    store = new Map([...store.entries()]);
+    console.debug('store', store);
+    subject.next(store);
+  },
+  update: (key, id, message) => {
+    const temp = store.get(key);
+    const temp1 = temp ? temp : new Set();
+    const idx = temp1.findIndex(obj => obj.id === id);
+    if (idx !== -1) {
+      temp1[idx] = message;
+      store.set(key, [...temp1]);
+      store = new Map([...store.entries()]);
+      subject.next(store);
+    }
+  },
+  clear: () => {
+    store = initialState;
+    subject.next(store);
+  },
+  initialState
 };
 export default iwsStore;
