43c0ef1552f332f3d078796dd5a2647bf976b3e1
balanka <batexy@gmail.com>
2024-02-14 15:08:13 +0100

    Refactored

--- src/views/base/Components/ComboBox.js
+++ src/views/base/Components/ComboBox.js
@@ -4,35 +4,40 @@ import Autocomplete from '@mui/material/Autocomplete'
 import { styled } from '@mui/material/styles'
 import { sortById, sortByName } from '../utils/utils'
 
-const StyledAutocomplete = styled(Autocomplete)({
-  '& .MuiInputLabel-outlined:not(.MuiInputLabel-shrink)': {
-    // Default transform is "translate(14px, 20px) scale(1)""
-    // This lines up the label with the initial cursor position in the input
-    // after changing its padding-left.
-    transform: 'translate(34px, 20px) scale(1);',
-  },
-  '&.Mui-focused .MuiInputLabel-outlined': {
-    color: 'purple',
-  },
-  '& .MuiAutocomplete-inputRoot': {
-    color: 'purple',
-    // This matches the specificity of the default styles at https://github.com/mui-org/material-ui/blob/v4.11.3/packages/material-ui-lab/src/Autocomplete/Autocomplete.js#L90
-    '&[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-of-type': {
-      // Default left padding is 6px
-      paddingLeft: 26,
+const StyledAutocomplete = styled(Autocomplete, {
+  shouldForwardProp: (props) => props !== 'primary',
+})((props) => {
+  return {
+    '& .MuiInputLabel-outlined:not(.MuiInputLabel-shrink)': {
+      // Default transform is "translate(14px, 20px) scale(1)""
+      // This lines up the label with the initial cursor position in the input
+      // after changing its padding-left.
+      transform: 'translate(34px, 20px) scale(2);',
     },
-    '& .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'white',
-      // borderColor: '#1E5EF3',
+    '&.Mui-focused .MuiInputLabel-outlined': {
+      // color: 'purple',
     },
-    '&:hover .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'red',
-      height: '30',
+    '& .MuiAutocomplete-inputRoot': {
+      // color: 'purple',
+      backgroundColor: props.disabled ? '#e3e7e7' : '#ffffff',
+      borderRadius: 5,
+      // This matches the specificity of the default styles at https://github.com/mui-org/material-ui/blob/v4.11.3/packages/material-ui-lab/src/Autocomplete/Autocomplete.js#L90
+      '&[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-of-type': {
+        // Default left padding is 6px
+        // paddingLeft: 16,
+      },
+      '& .MuiOutlinedInput-notchedOutline': {
+        borderColor: 'white',
+      },
+      '&:hover .MuiOutlinedInput-notchedOutline': {
+        borderColor: 'red',
+        height: '30',
+      },
+      '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+        borderColor: 'purple',
+      },
     },
-    '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'purple',
-    },
-  },
+  }
 })
 
 export default function ComboBox(props) {
@@ -86,6 +91,7 @@ export default function ComboBox(props) {
             variant="standard"
             size="small"
             inputRef={entryRef}
+            //InputProps={{ sx: { borderRadius: 2 } }}
             onChange={(e) => {
               entryRef.current.value = e.target.value
             }}
