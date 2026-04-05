87781eec4e4801c369d0b6117dceeee289cfb8ec
balanka <batexy@gmail.com>
2026-04-05 15:40:54 +0200

    added

---
+++ src/form/RealEstateForm.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {AllCommunityModule, ClientSideRowModelModule, ColDef, ModuleRegistry} from 'ag-grid-community'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-quartz.css'
+import Grid from 'react-fast-grid'
+
+// @ts-ignore
+import type {RowSelectedEvent} from 'ag-grid-community/dist/types/src/events'
+import {MasterfilesForm} from './FormsProps'
+import {MASTERFILE} from './Menu'
+import {masterfileColumnDefs} from '../ColumnsDefs.ts'
+import useForm from './UseForm.ts'
+import UseMasterfileForm from './UseMasterfileForm.ts'
+import {styles} from './BasicTreeTableProps.tsx'
+
+
+ModuleRegistry.registerModules([AllCommunityModule, ClientSideRowModelModule,])
+ const RealEstateForm = () => {
+     const [{t, module_}]  = useForm()
+     const height = 33
+     const current_ =  module_.state[0]
+     const minHeight = 450
+     const maxHeight = 700
+     const colDef:ColDef[]= masterfileColumnDefs(t)
+     const [{header, body, disable, table, state, visible, current, setCurrent}] = UseMasterfileForm(current_,  colDef, MASTERFILE.realEstate)
+     const mainForm = MasterfilesForm({collapse:state.collapse,  current:current??current_, setCurrent:setCurrent
+     , disable:disable, height:height, accData:[], t:t, fieldName:t('common.parent'), propertyName:'parent'})
+    return (
+      <>
+       {header}
+        {body??mainForm}
+       <Grid item style={{...styles.outer0, paddingTop:15, height: state.collapse?minHeight:maxHeight, display:visible?'':'none'}}>
+         {table}
+       </Grid>
+     </>
+   )
+}
+export default  RealEstateForm
---
+++ src/form/RoomForm.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react'
+import {AllCommunityModule, ClientSideRowModelModule, ColDef, ModuleRegistry} from 'ag-grid-community'
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-quartz.css'
+import Grid from 'react-fast-grid'
+
+// @ts-ignore
+import type {RowSelectedEvent} from 'ag-grid-community/dist/types/src/events'
+import {MasterfilesForm} from './FormsProps'
+
+import {MASTERFILE} from './Menu'
+import {masterfileColumnDefs} from '../ColumnsDefs.ts'
+import useForm from './UseForm.ts'
+import UseMasterfileForm from './UseMasterfileForm.ts'
+import {styles} from './BasicTreeTableProps.tsx'
+import iwsStore from "../utils/Store.tsx";
+import {Get} from "./CrudController.ts";
+import {IMasterfile2} from "../Models.ts";
+import {formEnum} from "../utils/FormEnum.tsx";
+
+
+ModuleRegistry.registerModules([AllCommunityModule, ClientSideRowModelModule,])
+ const RoomForm = () => {
+     const [{profile, t, company, module_}]  = useForm()
+    const {token} = profile
+    const url = MASTERFILE.apartment
+    const ctx =  `${url}/${formEnum.APARTMENT}/${company}`
+    const [, setIwsState] = useState(iwsStore.initialState)
+    const [accData, setAccData] = useState<IMasterfile2[]>([])
+     const height = 33
+     const current_ =  module_.state[0]
+     const minHeight = 450
+     const maxHeight = 700
+     const colDef:ColDef[]= masterfileColumnDefs(t)
+   useEffect(() => {
+     iwsStore.subscribe(setIwsState)
+     Get(ctx, token, formEnum.APARTMENT, setAccData)
+     setCurrent(current_)
+   }, [current_])
+   console.log('ctx')
+   const [{header, body, table, disable,  state, visible, current, setCurrent}] = UseMasterfileForm(current_, colDef, MASTERFILE.room)
+   const mainForm = MasterfilesForm({collapse:state.collapse,  current:current??current_, setCurrent:setCurrent
+     , disable:disable, height:height, accData:accData, t:t
+     , fieldName:t('common.parent'), propertyName:'parent'})
+    return (
+      <>
+       {header}
+        {body??mainForm}
+       <Grid item style={{...styles.outer0, paddingTop:15, height: state.collapse?minHeight:maxHeight, display:visible?'':'none'}}>
+         {table}
+       </Grid>
+     </>
+   )
+}
+export default  RoomForm
