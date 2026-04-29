4082340129f2a92a42c6fb88d29b77091c14e8a3
balanka <batexy@gmail.com>
2026-04-29 23:15:02 +0200

    updated

---
+++ public/env-config.js
@@ -0,0 +1,12 @@
+// public/env-config.js
+window._env_ = {
+  REACT_APP_API_BASE: "/api",
+  API_URL: "http://localhost:8080",   // or http://192.168.178.102:8080 if backend runs separately
+  API_PORT: "8080",
+  REACT_APP_HOST_IP_ADDRESS: "localhost",
+  REACT_APP_PORT: "8080",
+  REACT_WEB_HOST_IP_ADDRESS: "localhost",
+  NODE_ENV: "development",
+  SCHEME: "http",
+  REACT_APP_VERSION: "dev"
+};
